e0433238924da6c05eba4106067c6827c42ceb30
zhukova_oksana <ksy2905@gmail.com>
2022-08-09 08:15:00 +0300

    edit task02.js while

--- js/Task02.js
+++ js/Task02.js
@@ -2,7 +2,13 @@
 
 const isPrime = (number) => {
   let plain;
-  for (let i = 1; i <= number; i +=1) {
+  let i = 0;
+  while (i <= number) {
+    if (number === 1) {
+      plain = false;
+      break;
+    }
+
     if ((number % i) === 0) {
       if ((i === 1) || (i === number)) {
         plain = true;
@@ -11,9 +17,11 @@ const isPrime = (number) => {
         break;
       }
     }
+
+    i += 1;
   }
   return plain;
 }
 
-const simple = isPrime(11);
+const simple = isPrime(13);
 console.log(simple);
